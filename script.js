aaf24f1de5d45d7245aea9d1e70e4c1e8662bd2b
guyVicky <vikrantsingh1889@gmail.com>
2022-12-14 06:27:05 +0530

    functioning copy

--- script.js
+++ script.js
@@ -3,10 +3,40 @@ console.log("yo");
 const org = ["1", "1", "1", "1", "1", "1", "1", "1", "1"];
 let pieces = ["1", "1", "1", "1", "1", "1", "1", "1", "1"];
 
-let turn = 1; //1 means player 1...0 means 2
+let turn = 0; //0 means player 1...1 means 2
 
 function checkVictory() {
-  console.log("💨");
+  let flag = 0;
+  for (let i = 0; i < 9; i += 3) {
+    if (
+      pieces[i] === pieces[i + 1] &&
+      pieces[i + 1] === pieces[i + 2] &&
+      pieces[i] !== "1"
+    ) {
+      flag = 1;
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    if (
+      pieces[i] === pieces[i + 3] &&
+      pieces[i + 3] === pieces[i + 6] &&
+      pieces[i] !== "1"
+    ) {
+      flag = 1;
+    }
+  }
+  if (
+    (pieces[0] === pieces[4] && pieces[4] === pieces[8] && pieces[0] !== "1") ||
+    (pieces[2] === pieces[4] && pieces[4] === pieces[6] && pieces[2] !== "1")
+  ) {
+    flag = 1;
+  }
+  if (flag === 0) {
+    return;
+  }
+  let curpl = turn ? "1" : "2";
+  alert("Player " + curpl + " wins!");
+  resetBoard();
 }
 
 function resetBoard() {
@@ -18,15 +48,16 @@ function updateBoard() {
   for (let i = 0; i < 9; i++) {
     if (pieces[i] != "1") {
       if (pieces[i] == "O") {
-        document.getElementById(i).style.backgroundColor = "#a1eeff";
+        document.getElementById(i).style.backgroundImage =
+          "url('./assets/noughts.png')";
       } else {
-        document.getElementById(i).style.backgroundColor = "#4e1100";
+        document.getElementById(i).style.backgroundImage =
+          "url('./assets/crosses.png')";
       }
     } else {
-      document.getElementById(i).style.backgroundColor = "#ffffff";
+      document.getElementById(i).style.backgroundImage = "";
     }
   }
-  checkVictory();
 }
 
 function changeValue(c) {
@@ -45,6 +76,7 @@ function genCells() {
     cell.addEventListener("click", (event) => {
       changeValue(event.target);
       updateBoard();
+      checkVictory();
     });
     board.appendChild(cell);
   }
